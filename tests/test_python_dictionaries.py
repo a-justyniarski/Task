6b8ee03e858785ec20bf11d4773056588c35f419
ajusyniarski <ajustyniarski@gmail.com>
2022-09-05 10:57:11 +0200

    added comment

--- tests/test_python_dictionaries.py
+++ tests/test_python_dictionaries.py
@@ -4,7 +4,7 @@ import task_dictionary_quicksort, task_dictionary_heapsort
 
 PARAMETERS = 'dictionary'
 # Dictionaries: Already sorted, sorted reverse, with two values repeated n-times, duplicate of dictionary,
-# dictionary with same value. I do not check stability of sort algorithms.
+# dictionary with same value, duplicated keys with updated values. I do not check stability of sort algorithms.
 DICTIONARY = (
     ({"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8}),
     ({"a": 6, "b": 5, "c": 8, "d": 5, "e": 6, "f": 5, "g": 43, "h": 1}),
@@ -13,6 +13,8 @@ DICTIONARY = (
     ({"a": 6, "b": 5, "c": 8, "d": 5, "e": 12, "f": 3, "g": 43, "h": 1}),
     ({"a": 1, "b": 1, "c": 1, "d": 1, "e": 1, "f": 1, "g": 1, "h": 1}),
     ({"a": 6, "b": 5, "c": 8, "d": 5, "e": 12, "f": 3, "g": 43, "h": 1,
+      "a": 6, "b": 5, "c": 8, "d": 5, "e": 12, "f": 3, "g": 43, "h": 1}),
+    ({"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8,
       "a": 6, "b": 5, "c": 8, "d": 5, "e": 12, "f": 3, "g": 43, "h": 1}),
     ({}),
 )
